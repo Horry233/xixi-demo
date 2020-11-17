3cf669d701f837a3fc931da1e79405245964a9ee
HORRY <308366974@qq.com>
2020-11-18 00:25:40 +0800

    使用 chai .expect 添加四个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -16,3 +16,96 @@ new Vue({
     loading3: false
   }
 })
+
+//单元测试
+import chai from 'chai'
+
+const expect = chai.expect
+// --------------------测试是否能显示图标
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let herf = useElement.getAttribute('xlink:href')
+  // console.log(useElement);
+  expect(herf).to.eq('#x-setting')
+  
+  vm.$el.remove()
+  vm.$destroy()
+}
+// ------------------测试loading和其他图标同时出现
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let herf = useElement.getAttribute('xlink:href')
+  expect(herf).to.eq('#x-loading')
+  
+  vm.$el.remove()
+  vm.$destroy()
+}
+// -------------------------测试图标位置
+{
+  const div = document.createElement('div')
+  document.body.append(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg) //解构赋值拿order出来
+  expect(order).to.eq('1') //css里面的数字都是字符串
+  
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.append(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  
+  vm.$el.remove()
+  vm.$destroy()
+}
+// ----------------------测试click事件
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function(){
+    console.log(1)
+  })
+  let button = vm.$el
+  button.click()
+  
+  vm.$el.remove()
+  vm.$destroy()
+}
