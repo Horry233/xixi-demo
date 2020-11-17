ddd13bc921e152949ae135c8835c13933b2df4a5
HORRY <308366974@qq.com>
2020-11-17 21:08:50 +0800

    检测button-group的子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,7 +6,14 @@
 
 <script>
 export default {
-
+  mounted() {
+      for(let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase()
+        if(node.nodeName !== 'BUTTON') {
+          console.warn(`x-button-group 里的子元素应该全是 x-button,但是你写的是${name}`)
+        }
+      }
+    }
 }
 </script>
 
