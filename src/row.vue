b715a2117adce9170fc8bcece20e2ac3d68c8ae5
HORRY <308366974@qq.com>
2020-11-24 20:32:03 +0800

    完成了 row 和 col 的雏形

---
+++ src/row.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XiRow'
+
+};
+</script>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+}
+</style>
+
