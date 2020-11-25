f62c2b212cdb6715c22449e45ed3fdd0385e23c8
HORRY <308366974@qq.com>
2020-11-25 21:19:47 +0800

    col的响应式 默认是phone

--- src/col.vue
+++ src/col.vue
@@ -26,7 +26,6 @@ export default {
     offset: {
       type: [Number, String],
     },
-    phone: { type: Object, validator },
     ipad: { type: Object, validator },
     narrowPc: { type: Object, validator },
     pc: { type: Object, validator },
@@ -49,12 +48,10 @@ export default {
       };
     },
     colClass() {
-      let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
-      let phoneClass = []
+      let {span, offset , ipad, narrowPc, pc, widePc} = this
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-          ... (phone ? [`col-phone-${phone.span}`] : []),
           ... (ipad ? [`col-ipad-${ipad.span}`] : []),
           ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
           ... (pc ? [`col-pc-${pc.span}`] : []),
@@ -85,20 +82,7 @@ export default {
     }
   }
   //响应式
-  @media (max-width: 576px) {
-    $class-prefix: col-phone-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        width: ($n / 24) * 100%;
-      }
-    }
-    $class-prefix: offset-phone-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        margin-left: ($n / 24) * 100%;
-      }
-    }
-  }
+  
   @media (min-width: 577px) and (max-width: 768px) {
     $class-prefix: col-ipad-;
     @for $n from 1 through 24 {
