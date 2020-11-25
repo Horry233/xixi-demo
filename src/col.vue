d54d364623262083c2fbfb7b4ca45af96e3e3926
HORRY <308366974@qq.com>
2020-11-25 20:28:26 +0800

    初步实现响应式(有BUG)

--- src/col.vue
+++ src/col.vue
@@ -1,52 +1,75 @@
 <template>
-  <div class="col" :class="colClass"
-  :style="colStyle">
-  <!-- <div style="border: 1px solid green;height: 100px"> -->
-    <slot></slot>
-  <!-- </div> -->
+  <div class="col" :class="colClass" :style="colStyle">
+    <div style="border: 1px solid green; height: 100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value);
+  let valid = true;
+  keys.forEach((key) => {
+    if (!["span", "offset"].includes(key)) {
+      valid = false;
+    }
+  });
+  return valid;
+};
 export default {
   name: "XiCol",
   props: {
     span: {
-      type: [Number,String]
+      type: [Number, String],
     },
     offset: {
-      type: [Number,String]
-    }
+      type: [Number, String],
+    },
+    phone: { type: Object, validator },
+    ipad: { type: Object, validator },
+    narrowPc: { type: Object, validator },
+    pc: { type: Object, validator },
+    widePc: { type: Object, validator },
   },
   data() {
     return {
-      gutter: 0
-    }
+      gutter: 0,
+    };
   },
   computed: {
     /* 
     因为gutter是从row传过来的，他是在mounted之后才会传过来，放在data里面的话是created的时候被读取，
     读取之后便不会再改变，所以得不到正确的gutter
     */
-    colStyle() { 
+    colStyle() {
       return {
-        paddingLeft: this.gutter/2 + 'px', 
-        paddingRight: this.gutter/2 + 'px'
-      }
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px",
+      };
     },
     colClass() {
-      let {span,offset} = this
-      return [span && `col-${span}`, offset && `offset-${offset}`]
-    }
-  }
+      let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
+      let phoneClass = []
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+          ... (phone && [`col-phone-${phone.span}`]),
+          ... (ipad && [`col-ipad-${ipad.span}`]),
+          ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+          ... (pc && [`col-pc-${pc.span}`]),
+          ... (widePc && [`col-wide-pc-${widePc.span}`]),
+      ]
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .col {
-  // height: 100px;
-  // background-color: skyblue;  
-  // width: 50%;
+  height: 100px;
+  // background-color: skyblue;
+  width: 50%;
   // border: 1px solid red;
 
   $class-prefix: col-;
@@ -59,6 +82,77 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: ($n / 24) * 100%;
+    }
+  }
+  //响应式
+  @media (max-width: 576px) {
+    $class-prefix: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1201px) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
     }
   }
 }
