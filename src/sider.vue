501cbb7882e407038917534ee70dfb52bd74cdb7
HORRY <308366974@qq.com>
2020-11-27 17:46:38 +0800

    布局基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="sider">
+  <slot></slot>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'XiSider'
+}
+</script>
+
+<style scoped lang="scss">
+.sider {
+  
+}
+</style>
