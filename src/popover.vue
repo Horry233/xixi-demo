9f142a2f3bc6d0ba8128c2814d13819f6aeb5ad2
HORRY <308366974@qq.com>
2020-12-10 15:56:18 +0800

    四个方向的popover完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="popover" @click.stop="onClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" 
+    :class="{[`position-${position}`] : true}">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
@@ -17,12 +18,37 @@ export default {
       visible: false,
     }
   },
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top','bottom','left','right'].indexOf(value) >= 0
+      }
+    }
+  },
   methods: {
     positionContent() {
-      document.body.appendChild(this.$refs.contentWrapper)
-      let {width,height,top,left,} = this.$refs.triggerWrapper.getBoundingClientRect()
-      this.$refs.contentWrapper.style.left = left + window.scrollX + "px"
-      this.$refs.contentWrapper.style.top = top + window.scrollY + "px"     
+      const {contentWrapper,triggerWrapper} = this.$refs
+      document.body.appendChild(contentWrapper)
+      let {width,height,top,left,} = triggerWrapper.getBoundingClientRect()
+      if(this.position === 'top') {
+        contentWrapper.style.left = left + window.scrollX + "px"
+        contentWrapper.style.top = top + window.scrollY + "px"    
+      }else if(this.position === 'bottom') {
+        contentWrapper.style.left = left + window.scrollX + "px"
+        contentWrapper.style.top = top + height + window.scrollY + "px"    
+      }else if(this.position === 'left') {
+        contentWrapper.style.left = left + window.scrollX + "px"
+        let {height: height2} = contentWrapper.getBoundingClientRect()
+        contentWrapper.style.top = top + window.scrollY +
+        (height-height2) / 2 + "px"
+      }else if(this.position === 'right') {
+        contentWrapper.style.left = left + width + window.scrollX + "px"
+        let {height: height2} = contentWrapper.getBoundingClientRect()
+        contentWrapper.style.top = top + window.scrollY +
+        (height-height2) / 2 + "px"
+      }
     },
     onClickDocument(e) {
         //点击的是popover或者popover里面
@@ -75,28 +101,77 @@ $border-radius: 4px;
   border-radius: $border-radius;
   // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
   filter: drop-shadow(0 1px 1px rgba(88, 78, 78, 0.5));
-  transform: translateY(-100%);
-  margin-top: -10px;
   padding: .5em 1em;
   max-width: 20em;
   word-break: break-all;
   background-color: white;
   &::before,&::after {
     position: absolute;
-    left: 10px;
     content: '';
     display: block;
-    border: 10px solid transparent;
+    border: 10px solid transparent; 
     width: 0;
     height: 0;
+   }
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before,&::after {
+      left: 10px;
+    }
+    &::before {
+      top: 100%;
+      border-top-color: #333;
+    }
+    &::after {
+      top: calc(100% - 1px);
+      border-top-color: white;
+    }
   }
-  &::before {
-    top: 100%;
-    border-top-color: #333;
+  &.position-bottom {
+    margin-top: 10px;
+    &::before,&::after {
+      left: 10px;
+    }
+    &::before {
+      bottom: 100%;
+      border-bottom-color: #333;
+    }
+    &::after {
+      bottom: calc(100% - 1px);
+      border-bottom-color: white;
+    }
   }
-  &::after {
-    top: calc(100% - 1px);
-    border-top-color: white;
+  &.position-left {
+    margin-left: -10px;
+    transform: translateX(-100%);
+    &::before,&::after {
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    &::before {
+      left: 100%;
+      border-left-color: #333;
+    }
+    &::after {
+      left: calc(100% - 1px);
+      border-left-color: white;
+    }
+  }
+  &.position-right {
+    margin-left: 10px;
+    &::before,&::after {
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    &::before {
+      right: 100%;
+      border-right-color: #333;
+    }
+    &::after {
+      right: calc(100% - 1px);
+      border-right-color: white;
+    }
   }
 }
 </style>
