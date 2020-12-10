f0d6271884741fe782f1c6e80939dcae86e26ddb
HORRY <308366974@qq.com>
2020-12-10 15:06:52 +0800

    添加popover默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block">
       <slot></slot>
     </span>
   </div>
@@ -62,6 +62,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -69,8 +71,32 @@ export default {
 }
 .content-wrapper {
   position: absolute;
-  border: 1px solid pink;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0 1px 1px rgba(88, 78, 78, 0.5));
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: .5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+  background-color: white;
+  &::before,&::after {
+    position: absolute;
+    left: 10px;
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
+  }
+  &::before {
+    top: 100%;
+    border-top-color: #333;
+  }
+  &::after {
+    top: calc(100% - 1px);
+    border-top-color: white;
+  }
 }
 </style>
