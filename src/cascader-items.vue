4da22baaa4ff82a521807f461ea81dbbde2f49a3
HORRY <308366974@qq.com>
2020-12-19 17:50:03 +0800

    改用vue-cli构建项目， 上一次add没commit 气死我了

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,53 +1,71 @@
 <template>
-  <div class="cascader-items">
+  <div class="cascaderItem" :style="{height: height}">
     <div class="left">
-      <div v-for="item in items" @click="leftSelected = item">
+      <div class="label" v-for="item in items" @click="leftSelected = item" :key="item.name">
         {{item.name}}
+        <icon class="icon" v-if="item.children" name="right"></icon>
       </div>
     </div>
     <div class="right" v-if="rightItems">
-      <cascader-items :items="rightItems"></cascader-items>
+      <cascader-items :items="rightItems" :height="height"></cascader-items>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name:  "cascader-items",
-  props: {
-    items: {
-      type: Array
-    }
-  },
-  data() {
-    return {
-      leftSelected: null,
-      
-    }
-  },
-  computed: {
-    rightItems() {
-      if(this.leftSelected && this.leftSelected.children) {
-        return this.leftSelected.children
-      }else {
-        return null
+  import Icon from './icon'
+  export default {
+    name: "cascaderItems",
+    components: {Icon},
+    props: {
+      items: {
+        type: Array
+      },
+      height: {
+        type: String
+      }
+    },
+    data () {
+      return {
+        leftSelected: null
+      }
+    },
+    computed: {
+      rightItems () {
+        if (this.leftSelected && this.leftSelected.children) {
+          return this.leftSelected.children
+        } else {
+          return null
+        }
       }
     }
   }
-}
 </script>
 
-<style lang="scss" scoped>
-.cascader-items {
-  display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
-  .left {
-    border: 1px solid red;
+<style scoped lang="scss">
+  @import "var";
+  .cascaderItem {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    height: 100px;
+    .left {
+ 
+      height: 100%;
+      padding: .3em 0;
+    }
+    .right {
+      height: 100%;
+      border-left: 1px solid $border-color-light;
+    }
+    .label {
+      padding: .3em 1em;
+      display: flex;
+      align-items: center;
+      .icon {
+        margin-left: 1em;
+        transform: scale(0.5);
+      }
+    }
   }
-  .right { 
-    margin-top: -1px;
-  }
-}
-
-</style>
+</style> 
--- src/cascader.vue
+++ src/cascader.vue
@@ -3,8 +3,8 @@
     <div class="trigger" @click="popoverVisible = !popoverVisible">
 
     </div>
-    <div class="popover" v-if="popoverVisible">
-      <cascader-items :items="source"></cascader-items>
+    <div class="popover-wrapper" v-if="popoverVisible">
+      <cascader-items :items="source" class="popover" :height="popoverHeight"></cascader-items>
     </div>
   </div>
 </template>
@@ -17,6 +17,9 @@ export default {
   props: {
     source: {
       type: Array,
+    },
+    popoverHeight: {
+    type: String
     }
   },
   data() {
@@ -29,13 +32,18 @@ export default {
 
 
 <style lang="scss" scoped>
-@import "var";
-.cascader {
-  .trigger {
-    border: 1px solid red;
-    height: 32px;
-    width: 200px;
+  @import "var";
+  .cascader {
+    position: relative;
+    .trigger {
+      height: 32px;
+      width: 100px;
+      border: 1px solid black;
+    }
+    .popover-wrapper {
+      position: absolute; top: 100%; left: 0; background: white; display: flex;
+      @extend .box-shadow;
+    }
   }
-}
 
 </style>
