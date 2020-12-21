60b9495234b9b7533596ee8f56afcbb43a11b346
HORRY <308366974@qq.com>
2020-12-21 21:33:22 +0800

    rightitems渲染有bug

--- src/demo.vue
+++ src/demo.vue
@@ -1,9 +1,16 @@
 <template>
-  <div style="margin:100px">
-    <p>11111</p>
-    <x-cascader :source="source" popover-height="200px"
-      :selected.sync="selected"></x-cascader>
-    <p>22222</p>
+  <div style="margin:20px">
+    <p>{{selected &&selected[0] &&selected[0].name || '空'}}</p>
+    <p>{{selected &&selected[1] &&selected[1].name || '空'}}</p>
+    <p>{{selected &&selected[2] &&selected[2].name || '空'}}</p>
+
+    <div style="margin:100px">
+      <x-cascader :source.sync="source" popover-height="200px"
+      @update:source="onUpdateSource"
+      @update:selected="onUpdateSelected"
+      :selected.sync="selected" :load-data="loadData"></x-cascader>
+    </div>
+
   </div>
 </template>
 <script>
@@ -12,9 +19,14 @@
   import db from './db'
   
   function ajax (parentId = 0) {
-    return db.filter((item) => item.parent_id == parentId)
+      return new Promise((success, fail) => {
+      setTimeout(() => {
+        let result = db.filter((item) => item.parent_id == parentId)
+        success(result)
+      }, 300)
+    })
   }
-  console.log(ajax())
+
   export default {
     name: "demo",
     components: {
@@ -24,8 +36,30 @@
     data () {
       return {
         selected: [],
-        source: ajax()
+        source: []
       };
+    },
+        created () {
+      ajax(0).then(result => {
+        this.source = result
+      })
+    },
+    methods: {
+      loadData ({id}, updateSource) {
+        ajax(id).then(result => {
+          updateSource(result) // 回调:把别人传给我的函数调用一下
+        })
+      },
+      xxx () {
+        ajax(this.selected[0].id).then(result => {
+          let lastLevelSelected = this.source.filter(item => item.id === this.selected[0].id)[0]
+          this.$set(lastLevelSelected, 'children', result)
+        })
+      },
+      onUpdateSource () {
+      },
+      onUpdateSelected () {
+      }
     }
   };
 </script>
